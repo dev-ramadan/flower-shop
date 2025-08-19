463d96040ef867c9ba8d25c1dde29b5954327122
dev-ramadan <ganna123adam@gmail.com>
2025-08-19 21:21:24 +0300

    customer secation

--- Js/main.js
+++ Js/main.js
@@ -42,6 +42,52 @@ document.addEventListener("DOMContentLoaded", () => {
     $("#popupPrice").text(`$${flowerData.price.toFixed(2)}`);
     $("#popup").fadeIn();;
   };
+
+
+  // customer slider bt Swiper JS
+  const customerReviwe = async () => {
+    try {
+      const getReview = await fetch('../Js/customer.json');
+      const review = await getReview.json();
+      let swiperWrapper = document.querySelector(".swiper-wrapper");
+      review.forEach(item => {
+        let card = document.createElement("div");
+        card.classList.add("swiper-slide");
+        card.innerHTML = `
+                           <div class=" p-8 shadow-xl">
+                              <p class="mb-6 text-lg desc"> ${item.description} </p>
+                           <div class="flex items-center">
+                             <img src="${item.image}" class="w-14 h-14 rounded-full mr-4">
+                           <div>
+                             <h4 class="font-bold text-yellow-400 text-xl">${item.name}</h4>
+                             <p class="text-sm text-gray-500">${item.jop}</p>
+                           </div>
+                           </div>
+                           </div>
+      `;
+        swiperWrapper.appendChild(card);
+      });
+
+
+      new Swiper(".mySwiper", {
+        loop: true,
+        autoplay: { delay: 5000 },
+        pagination: { el: ".swiper-pagination", clickable: true },
+        navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+        slidesPerView: 1,
+        spaceBetween: 20,
+        breakpoints: {
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 }
+        }
+      });
+
+    } catch (error) {
+      console.error("Error loading reviews:", error);
+    }
+  };
+
+  customerReviwe();
 });
 
 
