abe371521b8f09ada04eb5e259371a7e6619d6f2
dev-ramadan <ganna123adam@gmail.com>
2025-09-06 19:33:20 +0300

    UPDATE

--- Src/Js/cart.js
+++ Src/Js/cart.js
@@ -1,28 +1,28 @@
 // cart
 
 document.addEventListener("DOMContentLoaded", () => {
-const cartRender = () => {
+  const cartRender = () => {
     let cartItems = JSON.parse(localStorage.getItem("cart")) || [];
-  let container = document.getElementById("cart-container");
+    let container = document.getElementById("cart-container");
 
-  if (cartItems.length === 0) {
-    container.innerHTML = "<p>Cart is empty</p>";
-    return;
-  }
+    if (cartItems.length === 0) {
+      container.innerHTML = "<p>Cart is empty</p>";
+      return;
+    }
 
-  cartItems.forEach((item) => {
-    let card = document.createElement("div");
-    card.classList.add(
-      "bg-white",
-      "dark:bg-gray-500",
-      "w-full",
-      "p-4",
-      "rounded-lg",
-      "flex",
-      "items-center",
-      "flex-col"
-    );
-    card.innerHTML = `
+    cartItems.forEach((item) => {
+      let card = document.createElement("div");
+      card.classList.add(
+        "bg-white",
+        "dark:bg-gray-500",
+        "w-full",
+        "p-4",
+        "rounded-lg",
+        "flex",
+        "items-center",
+        "flex-col"
+      );
+      card.innerHTML = `
       <img src="${item.image}" class="w-60 h-60 rounded-lg">
       <h3 class= "text-lg text-red-500 dark:text-white">${item.name}</h3>
       <p>$${item.price}</p>
@@ -34,35 +34,39 @@ const cartRender = () => {
                 </span>
       <button id='remove'>Remove</button>
       `;
-    container.appendChild(card);
-    let qty = card.querySelector(".qty");
-    let setQty = card.querySelector("#setQty");
-    let plus = card.querySelector(".plus");
-    let mins = card.querySelector(".mins");
-    let remove = card.querySelector("#remove");
-    remove.addEventListener("click" , ()=>{
-      let newCartItems = cartItems.filter(product => product.id != item.id );
-      localStorage.setItem("cart", JSON.stringify(newCartItems))
-      cartRender();
-      updateCartCounter()
-    })
-    // controle in the qty
+      container.appendChild(card);
+      let qty = card.querySelector(".qty");
+      let setQty = card.querySelector("#setQty");
+      let plus = card.querySelector(".plus");
+      let mins = card.querySelector(".mins");
+      let remove = card.querySelector("#remove");
+      remove.addEventListener("click", () => {
+        container.innerHTML = '';
+        let newCartItems = cartItems.filter(product => product.id !== item.id);
+        localStorage.setItem("cart", JSON.stringify(newCartItems))
+        cartRender();
+        updateCartCounter()
+      })
+      // controle in the qty
 
-    plus.addEventListener("click", () => {
-      setQty.value = Number(setQty.value) + 1;
-      localStorage.setItem("cart", JSON.stringify(cartItems, { item: item.quantity = setQty.value }));
-      qty.textContent = setQty.value
-    });
+      plus.addEventListener("click", () => {
+        setQty.value = Number(setQty.value) + 1;
+        item.quantity = Number(setQty.value); 
+        localStorage.setItem("cart", JSON.stringify(cartItems)); 
+        qty.textContent = setQty.value;
+      });
 
-    mins.addEventListener("click", () => {
-      if (setQty.value > 1) {
-        setQty.value = Number(setQty.value) - 1;
-        localStorage.setItem("cart", JSON.stringify(cartItems, { item: item.quantity = setQty.value }));
-      qty.textContent = setQty.value
-      }
+      mins.addEventListener("click", () => {
+        if (setQty.value > 1) {
+          setQty.value = Number(setQty.value) - 1;
+          item.quantity = Number(setQty.value);
+          localStorage.setItem("cart", JSON.stringify(cartItems));
+          qty.textContent = setQty.value;
+        }
+      });
+
+
     });
- 
-  });
-}
-cartRender()
+  }
+  cartRender()
 });
